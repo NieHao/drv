694a8f2ccc8f69e40f6929bfb806c0f704a1343e
NieHao <nienie1900@gmail.com>
2014-06-21 09:49:20 +0800

    2

--- 1,myvivi-出错.c
+++ 1,myvivi-出错.c
@@ -1,22 +1,28 @@
 
 #include <linux/module.h>
+#include <linux/delay.h>
 #include <linux/errno.h>
+#include <linux/fs.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/mm.h>
+#include <linux/ioport.h>
 #include <linux/init.h>
 #include <linux/sched.h>
-#include <linux/slab.h>
-#include <linux/font.h>
+#include <linux/pci.h>
+#include <linux/random.h>
+#include <linux/version.h>
 #include <linux/mutex.h>
 #include <linux/videodev2.h>
+#include <linux/dma-mapping.h>
+#include <linux/interrupt.h>
 #include <linux/kthread.h>
+#include <linux/highmem.h>
 #include <linux/freezer.h>
-#include <media/videobuf2-vmalloc.h>
+#include <media/videobuf-vmalloc.h>
 #include <media/v4l2-device.h>
 #include <media/v4l2-ioctl.h>
-#include <media/v4l2-ctrls.h>
-#include <media/v4l2-fh.h>
-#include <media/v4l2-event.h>
-#include <media/v4l2-common.h>
+#include "font.h"
 
 // 零，相关结构等等
 	/* 0.1,定义一个 video_device 结构体 */
@@ -32,9 +38,9 @@ static int myvivi_init(void)
 	// 1.2,设置
 
 	// 1.3,注册
-	/* 	参1:video_device结构体即myvivi_devcie.
-		参2，注册的设备的类型
-		参3，(0 == /dev/video0, 1 == /dev/video1, ... -1 == first free)
+	/* 	@1:video_device结构体即myvivi_devcie.
+		@2:注册的设备的类型
+		@3:(0 == /dev/video0, 1 == /dev/video1, ... -1 == first free)
 	*/
 	errno = video_register_device(myvivi_device,VFL_TYPE_GRABBER,-1);
 	return errno;
@@ -43,7 +49,7 @@ static int myvivi_init(void)
 // 二，出口函数
 static void myvivi_exit(void)
 {
-	/* 2.1, */
+	/* 2.1, 释放注册的结构体*/
 	video_unregister_device(myvivi_device);
 
 	/* 2.2,上面有分配，则出口函数就注销 */
